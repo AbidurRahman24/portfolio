6f18d5daedffd493b1321431b5c11c22c1f707ab
Niloy-Sumon <niloyabidur@gmail.com>
2021-05-23 14:40:34 +0600

    firebase install

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            <h3>Hello Contact Page</h3>
+        <div className='app'>
+            <form className='form'>
+                <h1>Contact Form</h1>
+                <label>Name:</label>
+                <input type="name" placeholder='Enter Your Name'/>
+                <label>Email</label>
+                <input type="email" placeholder='Enter Your Email' />
+                <label>Message</label>
+                <textarea name="textarea" id="" cols="30" rows="10" placeholder='Enter Your Massage'></textarea>
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     );
 };
--- src/component/NewsDetails/NewsDetails.js
+++ src/component/NewsDetails/NewsDetails.js
@@ -5,13 +5,13 @@ import { useParams } from 'react-router';
 const NewsDetails = () => {
    const {id} = useParams()
    const [news, setNews] = useState({})
-   console.log(id)
    useEffect(()=>{
-       const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${id}`
+       const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=4aad6d1b666c49e5b19ec854bdd20c47`
        fetch(url)
        .then(res => res.json())
        .then(data =>{
            setNews(data)
+           console.log(data)
        })
 
    },[])
